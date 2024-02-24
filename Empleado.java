69051961dd9f1959be00a1ca77518ef196f73035
Barrerajaime <160670377+Barrerajaime@users.noreply.github.com>
2024-02-23 20:12:46 -0500

    Create Empleado.java
    esta clase es el complemento final de la clase de  TiendaDeCafe.java y Producto.java

---
+++ Empleado.java
@@ -0,0 +1,13 @@
+public class Empleado {
+    private String id;
+    private String nombre;
+    private String cargo;
+    private double salario;
+
+    public Empleado(String id, String nombre, String cargo, double salario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+}
