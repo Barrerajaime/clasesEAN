aec169419de6268654600c16c6061a542cb62992
Barrerajaime <160670377+Barrerajaime@users.noreply.github.com>
2024-02-23 20:11:59 -0500

    Create Producto.java
    esta clase va en mano con la clase deTiendaDeCafe.java

---
+++ Producto.java
@@ -0,0 +1,14 @@
+public class Producto {
+    private int codigo = 101;
+    private String nombre = "Café Especial EAN";
+    private String categoria = "ESPRESSO";
+    private String descripcion = "Descripción detallada del producto";
+    private double precioCompra = 1.50;
+    private double precioVenta = 2.99;
+    private int stock = 100;
+    private Proveedor proveedor;
+
+    public Producto() {
+        // Constructor predeterminado
+    }
+}
